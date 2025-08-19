bedac88d9a54f1af7e9ee98ef2cbc47ccebac8c3
hidlestone <809566095@qq.com>
2025-08-19 11:30:38 +0800

    feat: kafka批量配置

---
+++ jeecg-boot/jeecg-server-cloud/jeecg-visual/jeecg-cloud-test/jeecg-cloud-test-kafka/src/main/java/org/jeecg/visual/kafka/consumer/BatchListener.java
@@ -0,0 +1,61 @@
+package org.jeecg.visual.kafka.consumer;
+
+import com.fasterxml.jackson.databind.deser.std.NumberDeserializers;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class BatchListener {
+    private static final Logger log = LoggerFactory.getLogger(BatchListener.class);
+
+    private Map consumerProps() {
+        Map props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.20.200:9092");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "15000");
+        // 一次拉取消息数量
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "10");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, NumberDeserializers.IntegerDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return props;
+    }
+
+    @Bean("batchContainerFactory")
+    public ConcurrentKafkaListenerContainerFactory listenerContainer() {
+        ConcurrentKafkaListenerContainerFactory container
+                = new ConcurrentKafkaListenerContainerFactory();
+        container.setConsumerFactory(new DefaultKafkaConsumerFactory(consumerProps()));
+        // 设置并发量，小于或等于 Topic 的分区数
+        container.setConcurrency(5);
+        // 必须 设置为批量监听
+        container.setBatchListener(true);
+        return container;
+    }
+
+    @Bean
+    public NewTopic batchTopic() {
+        return new NewTopic("topic.batch", 8, (short) 1);
+    }
+
+    @KafkaListener(id = "batchConsumer", clientIdPrefix = "batch"
+            , topics = {"topic.batch"}, containerFactory = "batchContainerFactory")
+    public void batchListener(List data) {
+        log.info("topic.batch  receive :");
+        for (Object s : data) {
+            log.info(s.toString());
+        }
+    }
+}
